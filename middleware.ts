244f9b129951a6e2628cbac58a6621e617b0be88
guidopastorino <guidopasto05@gmail.com>
2024-09-25 13:04:19 -0300

    Updating middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,29 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export const config = { matcher: ["/send-email", "/moments", "/new", "/privacy-policy", "/settings", "/reservations"] }
+export default withAuth(
+  function middleware(req) {
+    const token = req.nextauth.token;
+
+    // Si el usuario no está autenticado, redirigir al inicio de sesión
+    if (!token) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/"; // Redirigir al inicio de sesión
+      url.searchParams.set("callbackUrl", req.nextUrl.pathname); // Mantener la URL a la que intentaba acceder
+      return NextResponse.redirect(url);
+    }
+
+    // Si está autenticado, permitir acceso
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token, // Usuario autenticado si hay un token
+    },
+  }
+);
+
+// Configuración de las rutas que deben estar protegidas
+export const config = {
+  matcher: ["/send-email", "/new", "/privacy-policy", "/settings", "/reservations"],
+};
