f617d8c67702e7290aa19ff6265f50359c1584ed
guidopastorino <guidopasto05@gmail.com>
2024-10-02 21:31:53 -0300

    Styling main page

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,9 @@ const page = async () => {
   return (
     <div className="w-full border max-w-screen-lg mx-auto p-4">
       {/* añadir control de errores como pasarse de horas o horas negativas */}
+      {session?.user._id}
       <ReservationForm />
+      <img src="/cancha.webp" alt="Foto de cancha de Fútbol 5" />
     </div>
   );
 };
